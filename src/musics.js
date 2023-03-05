05673137e14558c1dd83f4d55ed384ec765f0be2
RezaDadashi <dadashireza313@gmail.com>
2023-03-05 12:48:03 +0330

    vue music player

---
+++ src/musics.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        source: './musics/1.mp3',
+        photo: './musics/1.jpg',
+        title: 'Music 1',
+    },
+    {
+        source: './musics/2.mp3',
+        photo: './musics/2.jpg',
+        title: 'Music 2',
+    },
+    {
+        source: './musics/3.mp3',
+        photo: './musics/3.jpg',
+        title: 'Music 3',
+    },
+]
